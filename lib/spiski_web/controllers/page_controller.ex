6513cefc095b751cb933c1b405146ee6d3099a5a
Mikhail Chuprynski <1m@tut.by>
2020-11-19 11:47:53 +0100

    UI

--- lib/spiski_web/controllers/page_controller.ex
+++ lib/spiski_web/controllers/page_controller.ex
@@ -2,20 +2,25 @@ defmodule SpiskiWeb.PageController do
   use SpiskiWeb, :controller
 
   def index(conn, params) do
-    name = params["search"]["name"]
-    raw_data = :ets.match_object(:db, {:_, :_, :_, :_, :_, name})
-    headers = :ets.lookup(:db, :headers)
-    raw_data
-
-    data = Enum.map(
-             raw_data,
-             fn e ->
-               Enum.zip(headers[:headers], Tuple.to_list(e))
-               |> Enum.into(%{})
-               |> Map.drop(["", "#"])
-             end
-           ) |> IO.inspect
-
+    name = params["search"]["name"] |> String.trim
+    data = search(name)
+    data |> IO.inspect
     render(conn, "index.html", name: name, data: data)
   end
+
+  defp search(name) when name != "" do
+    raw_data = :ets.match_object(:db, {:_, :_, :_, :_, :_, name})
+    headers = :ets.lookup(:db, :headers)
+
+    data = Enum.map(
+      raw_data,
+      fn e ->
+        Enum.zip(headers[:headers], Tuple.to_list(e))
+        |> Enum.into(%{})
+        |> Map.drop(["", "#"])
+      end
+    )
+  end
+
+  defp search(_), do: []
 end
