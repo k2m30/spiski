269ba624965ef016207d1b0e4993656ca8c16f15
Mikhail Chuprynski <1m@tut.by>
2020-11-20 23:18:39 +0100

    several people, comma separated

--- lib/spiski_web/controllers/page_controller.ex
+++ lib/spiski_web/controllers/page_controller.ex
@@ -2,14 +2,30 @@ defmodule SpiskiWeb.PageController do
   use SpiskiWeb, :controller
 
   def index(conn, params) do
-    name = (params["search"]["name"] || "") |> String.split |> (List.first) || "" |> String.trim |> String.capitalize
-    data = search(name)
-    data |> IO.inspect
-    render(conn, "index.html", name: name, data: data, db_size: :ets.info(:db)[:size])
+    names = (params["search"]["names"] || "")
+            |> String.split(",")
+            |> Enum.map(
+                 &(
+                   &1
+                   |> String.trim
+                   |> String.split
+                   |> (List.first) || ""
+                   |> String.trim
+                   |> String.capitalize)
+               )
+            |> Enum.reject(&(&1 == ""))
+
+    data = names |> Enum.map(&(search(&1)))
+    Enum.zip(names, data) |> Enum.into(%{}) |> IO.inspect
+
+    render(conn, "index.html", names: names, data: Enum.zip(names, data) |> Enum.into(%{}), db_size: :ets.info(:db)[:size])
   end
 
   defp search(name) when name != "" do
-    raw_data = :ets.match_object(:db, {:_,:_,:_,:_,:_,:_,:_,:_,:_,:_,:_,:_,:_,:_,:_,:_,:_,:_,:_,:_,:_,:_, name})
+    raw_data = :ets.match_object(
+      :db,
+      {:_, :_, :_, :_, :_, :_, :_, :_, :_, :_, :_, :_, :_, :_, :_, :_, :_, :_, :_, :_, :_, :_, name}
+    )
     headers = :ets.lookup(:db, :headers)
 
     data = Enum.map(
